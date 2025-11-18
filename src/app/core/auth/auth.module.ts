cbc36b5f6a03d5648170ed19640c84a9c1630a08
Franco-Serra <frankmourice28@gmail.com>
2025-11-18 13:28:09 -0300

    listo para produccion

--- src/app/core/auth/auth.module.ts
+++ src/app/core/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from '../../featured/auth/login/login.component';
 
 const routes: Routes = [
   {
--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { User } from '../models/user.model';
+import { User } from '../../featured/users/models/user.model';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
